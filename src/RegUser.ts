81b9540127974e1f3ebdafc2b13c1bb20b712b4d
swampowl <florian.malitschenko@outlook.dem>
2021-07-14 17:33:27 +0200

    added userSwitchCase

--- src/RegUser.ts
+++ src/RegUser.ts
@@ -6,7 +6,7 @@ export class RegUser extends UnregUser {
 
 
     constructor() {
-        super(["4. view statistics of own questionaries", "5. create a new questionarie"]);
+        super(["<4> view statistics of own questionaries", "<5> create a new questionarie"]);
         console.log(this.initalStartOptions);
     }
 }
--- src/UnregUser.ts
+++ src/UnregUser.ts
@@ -5,7 +5,7 @@ import { UserManager } from "./UserManager";
 
 export class UnregUser {
 
-    public initalStartOptions: string[] = ["1. show ten questionaries", "2. search questionarie by name", "3. show amount of taken questionaries and their names"];
+    public initalStartOptions: string[] = ["<1> show ten questionaries", "<2> search questionarie by name", "<3> show amount of taken questionaries and their names"];
 
 
     constructor(additionalStartoptions?: string[]) {
@@ -20,5 +20,31 @@ export class UnregUser {
         console.clear();
         ConsoleHandling.printInput("Please choose a activity:");
         let answer: string = await ConsoleHandling.showPossibilities(this.initalStartOptions, "Your Statement:");
+        switch (answer) {
+            case "1":
+                console.clear();
+                console.log("CASE 1\n")
+                return;
+            case "2":
+                console.clear();
+                console.log("CASE 1\n")
+                return;
+            case "4":
+                console.clear();
+                console.log("CASE 4\n")
+                return;
+            case "4":
+                console.clear();
+                console.log("CASE 4\n")
+                return;
+            case "5":
+                console.clear();
+                console.log("CASE 5\n")
+                return;
+            default:
+                ConsoleHandling.printInput("please choose a siutable input.");
+                break;
+        }
+
     }
 }
